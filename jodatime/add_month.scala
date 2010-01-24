be71c14f428cb2ec4f53dc592f2a9c19e3b50ad0
Steven Shaw <steshaw@gmail.com>
2010-01-24 18:34:55 +1000

    Playing around with JodaTime

---
+++ jodatime/add_month.scala
@@ -0,0 +1,31 @@
+//
+// See what happens when 'add one month' is applied to a date at the end of the month. ie. 2009-12-31.
+// What happens is that it hits February which gets 2010-02-28 and then always the 28th is returned.
+// Similar "problems" would happend when passing through a month with the 30th.
+//
+
+import org.joda.time._
+
+def imperativeStyle() {
+  var dt = new DateTime("2009-12-31")
+  def go() {
+    (1 to 13).foreach {(n:Int) =>
+      println(n)
+      dt = dt.plusMonths(1)
+      println(dt.toString())
+    }
+  }
+  go()
+}
+
+def functionalStyle() {
+  def iter(i: Int, dt: DateTime) {
+    if (i <= 13) {
+      println(i)
+      val newDt = dt.plusMonths(1)
+      println(newDt.toString())
+      iter(i+1, newDt)
+    }
+  }
+  iter(1, new DateTime("2009-12-31"))
+}
---
+++ jodatime/add_month.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+scala -deprecation -classpath  ~/.m2/repository/joda-time/joda-time/1.6/joda-time-1.6.jar -i add_month.scala
