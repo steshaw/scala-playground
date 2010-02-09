f76a0778883223d5336aa617ed18f0b0f36dca46
Steven Shaw <steshaw@gmail.com>
2010-02-09 10:37:42 +1000

    Finally implemented State Monad but still don't understand it

--- state-monad/TreeNumberWithStateMonad.scala
+++ state-monad/TreeNumberWithStateMonad.scala
@@ -6,28 +6,24 @@ object TreeNumberWithStateMonad {
   final case class Leaf[A](a: A) extends Tree[A]
   final case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
-  // Failed attempt to implement the state monad thingo starting with Tony's code from the blog post.
-
+  // My implementation of the State monad following the initial template from Tony's post.
   trait State[S, A] {
     val s: Function[S, (S, A)]
-    def flatMap[B](f: A => Function[S, (S, B)]): Function[S, (S, B)] = x => s(x) match {
-        case (s, a) => f(a)(s)
+    def flatMap[B](f: A => State[S, B]): State[S, B] = new State[S, B] {
+      override val s: Function[S, (S, B)] = x => State.this.s(x) match {
+        case (s, a) => f(a).s(s)
+      }
     }
-    def map[B](f: A => B): Function[S, (S, B)] = s0 => s(s0) match {
-      case (s, a) => (s, f(a))
+    def map[B](f: A => B): State[S, B] = new State[S, B] {
+      override val s: Function[S, (S, B)] = s0 => State.this.s(s0) match {
+        case (s, a) => (s, f(a))
+      }
     }
   }
-
-/*
-  def mkState[S, A](f: S => (S, A)): State[S, A] = new State[S, A] {
-    def apply(s: S) = f(s)
-  }
-*/
 
   def init[S] = new State[S, S] {
     override val s: Function[S, (S, S)] = (init => (init, init))
   }
-
   def modify[S](f: S => S) = new State[S, S] {
     override val s: Function[S, (S, S)] = (init => (f(init), init))
   }
@@ -41,14 +37,34 @@ object TreeNumberWithStateMonad {
         yield Branch(lt, rt)
   }
 
+  type NumTree[A] = Tree[(A, Int)]
+
+  def number2[A](t: Tree[A]): State[Int, NumTree[A]] = t match {
+    case Leaf(x) =>
+      init[Int] flatMap ((s:Int) => modify[Int](1+) map ((_:Int) => Leaf((x, s))))
+    case Branch(l, r) =>
+      number(l) flatMap((lt:NumTree[A]) => number(r) map ((rt:NumTree[A]) => Branch(lt, rt)))
+  }
+
   def main(args: Array[String]) = {
     def go(tree: Tree[Int]) {
       println(tree)
-      println(number(tree) ! 10)
+      println(number(tree).s(10)._2)
+    }
+    def go2(tree: Tree[Int]) {
+      println(tree)
+      println(number2(tree).s(10)._2)
     }
 
-    go(Branch(Branch(Leaf(2), Leaf(6)), Leaf(9)))
-    go(Branch(Branch(Leaf(2), Branch(Leaf(4), Leaf(5))), Leaf(9)))
+    val t1 = Branch(Branch(Leaf(2), Leaf(6)), Leaf(9))
+    val t2 = Branch(Branch(Leaf(2), Branch(Leaf(4), Leaf(5))), Leaf(9))
+
+    go(t1)
+    go(t2)
+    println
+
+    go2(t1)
+    go2(t2)
   }
 
 }
