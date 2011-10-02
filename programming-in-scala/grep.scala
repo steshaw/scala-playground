3c2ab4a847353d1c2ec8be213c471e1a912c1769
Steven Shaw <steven@steshaw.org>
2011-10-02 17:14:12 +1000

    Use trimmed 'val'. Plus using braces means less semicolons

--- programming-in-scala/grep.scala
+++ programming-in-scala/grep.scala
@@ -3,11 +3,12 @@ val filesHere = new java.io.File(".").listFiles
 def fileLines(file: java.io.File) = scala.io.Source.fromFile(file).getLines.toList
 
 def grep(pattern: String) =
-  for(
+  for {
     file <- filesHere
-    if file.getName.endsWith(".scala");
+    if file.getName.endsWith(".scala")
     line <- fileLines(file)
-    if line.trim.matches(pattern)
-  ) println(file + ": " + line.trim)
+    trimmed = line.trim
+    if trimmed.matches(pattern)
+  } println(file + ": " + trimmed)
 
 grep("^.*gcd.*$")
