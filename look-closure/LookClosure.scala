ad9b77e197dd2c54be51fbffd363e106734e3927
Steven Shaw <steven@steshaw.org>
2011-10-03 14:07:46 +1000

    Couple more variants

--- look-closure/LookClosure.scala
+++ look-closure/LookClosure.scala
@@ -1,13 +1,49 @@
 import scala.collection.mutable.ListBuffer
 
-val delayedActions: ListBuffer[() => Unit] = ListBuffer()
-
-for (i <- 4 to 6) {
-  delayedActions += (() => println(i))
+def executeDelayedActions(delayedActions: Seq[() => Unit]) {
+  for (k <- 0 until delayedActions.length) {
+    delayedActions(k).apply()
+  }
 }
 
-println("all same = " + delayedActions.forall(_ == delayedActions(0)))
+{
+  // Fairly natural Scala version.
+  val delayedActions: ListBuffer[() => Unit] = ListBuffer()
 
-for (k <- 0 until delayedActions.length) {
-  delayedActions(k).apply()
+  for (i <- 4 to 6) {
+    delayedActions += (() => println(i))
+  }
+
+  println("all same = " + delayedActions.forall(_ == delayedActions(0)))
+
+  executeDelayedActions(delayedActions) // 4 5 6
+}
+{
+  // Imperative while loop Scala version.
+  val delayedActions: ListBuffer[() => Unit] = ListBuffer()
+
+  var i = 4
+  while (i < 7) {
+    delayedActions += (() => println(i))
+    i += 1
+  }
+
+  println("all same = " + delayedActions.forall(_ == delayedActions(0)))
+
+  executeDelayedActions(delayedActions) // 7 7 7
+}
+{
+  // Imperative Scala version with fix up.
+  val delayedActions: ListBuffer[() => Unit] = ListBuffer()
+
+  var i = 4
+  while (i < 7) {
+    var j = i
+    delayedActions += (() => println(j))
+    i += 1
+  }
+
+  println("all same = " + delayedActions.forall(_ == delayedActions(0)))
+
+  executeDelayedActions(delayedActions) // 7 7 7
 }
