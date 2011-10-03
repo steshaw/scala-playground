b7895f0ae9c24d31201d4c674ef3e16d04ee1301
Steven Shaw <steven@steshaw.org>
2011-10-03 16:48:34 +1000

    Try implicit + tail recursive loop

--- programming-in-scala/by-name.scala
+++ programming-in-scala/by-name.scala
@@ -4,3 +4,22 @@ def times(num: Int)(action: => Unit) = for (i <- 1 to num) action
 times(3) {
   println("hello")
 }
+
+class Times(num: Int) {
+  def times(action: => Unit) = for (i <- 1 to num) action
+}
+
+implicit def int2times(i: Int) = new Times(i)
+
+3 times {
+  println("hi")
+}
+
+def loop(action: => Unit) {
+  action
+  loop(action)
+}
+
+loop {
+  println("...")
+}
