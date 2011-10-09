1db3dbb27fa78699c9fe739e7f10de36ee0ea41f
Steven Shaw <steven@steshaw.org>
2011-10-09 17:43:18 +1000

    Add other simple examples. Show that parenthesis aren't necessarily always correct

--- programming-in-scala/expression-formatter/express.scala
+++ programming-in-scala/expression-formatter/express.scala
@@ -2,6 +2,8 @@ import org.stairwaybook.expr._
 
 object Express extends App {
   val f = new ExprFormatter
+
+
   val e1 = BinaryOp("*", BinaryOp("/", Number(1), Number(2)),
                          BinaryOp("+", Var("x"), Number(1)))
   val e2 = BinaryOp("+", BinaryOp("/", Var("x"), Number(2)),
@@ -10,5 +12,14 @@ object Express extends App {
 
   def show(e: Expr) = println(f.format(e) + "\n\n")
 
-  for (e <- Array(e1, e2, e3)) show(e)
+  val se1 = BinaryOp("-", Number(1),
+                          BinaryOp("-", Number(8), Number(7)))
+  val se2 = BinaryOp("-", BinaryOp("-", Number(1), Number(8)),
+                          Number(7))
+  val se3 = BinaryOp("+", BinaryOp("+", Number(1), Number(2)),
+                          Number(3))
+  val se4 = BinaryOp("+", Number(1),
+                          BinaryOp("+", Number(2), Number(3)))
+
+  for (e <- Array(e1, e2, e3, se1, se2, se3, se4)) show(e)
 }
