745c8e816ea366bd31c690e940c291c843a837f3
Steven Shaw <steven@steshaw.org>
2011-10-09 17:45:43 +1000

    Use foreach over for

--- programming-in-scala/expression-formatter/express.scala
+++ programming-in-scala/expression-formatter/express.scala
@@ -1,16 +1,12 @@
 import org.stairwaybook.expr._
 
 object Express extends App {
-  val f = new ExprFormatter
-
 
   val e1 = BinaryOp("*", BinaryOp("/", Number(1), Number(2)),
                          BinaryOp("+", Var("x"), Number(1)))
   val e2 = BinaryOp("+", BinaryOp("/", Var("x"), Number(2)),
                          BinaryOp("/", Number(1.5), Var("x")))
   val e3 = BinaryOp("/", e1, e2)
-
-  def show(e: Expr) = println(f.format(e) + "\n\n")
 
   val se1 = BinaryOp("-", Number(1),
                           BinaryOp("-", Number(8), Number(7)))
@@ -21,5 +17,9 @@ object Express extends App {
   val se4 = BinaryOp("+", Number(1),
                           BinaryOp("+", Number(2), Number(3)))
 
-  for (e <- Array(e1, e2, e3, se1, se2, se3, se4)) show(e)
+  val f = new ExprFormatter
+  def show(e: Expr) = println(f.format(e) + "\n\n")
+
+  val all = Seq(e1, e2, e3, se1, se2, se3, se4)
+  all foreach show
 }
