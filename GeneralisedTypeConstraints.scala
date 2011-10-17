bd041f8254b1a503baad74e1a18d8f5f50273a48
Steven Shaw <steven@steshaw.org>
2011-10-17 12:13:14 +1000

    Try difference generalised type constraints

--- GeneralisedTypeConstraints.scala
+++ GeneralisedTypeConstraints.scala
@@ -1,8 +1,17 @@
 //
-// Found at http://stackoverflow.com/questions/3427345/what-do-and-mean-in-scala-2-8-and-where-are-they-documented/3427759#3427759
+// Adapted from http://stackoverflow.com/questions/3427345/what-do-and-mean-in-scala-2-8-and-where-are-they-documented/3427759#3427759
 //
 
+implicit def int2string(i: Int) = i.toString
+
 case class Foo[A](a:A) { // 'A' can be substituted with any type
-  // getStringLength can only be used if this is a Foo[String]
-  def getStringLength(implicit evidence: A =:= String) = a.length
+  // stringLength can only be used if this is a Foo[String]
+  def stringLength(implicit evidence: A =:= String) = a.length
+
+  // stringLengthSubtype can be used if A is a subtype of CharSequence such as Foo[String]
+  def stringLengthSubtype(implicit evidence: A <:< CharSequence) = a.length
+
+  // stringLengthView can be used if A is implicitly convertible to a CharSequence, 
+  // such as Foo[Int], given int2string above
+  def stringLengthView(implicit evidence: A <%< String) = a.length
 }
