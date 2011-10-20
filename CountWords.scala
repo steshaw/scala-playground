2e18c3d26e9690c4ccf12731e218ee4e50d44ed7
Steven Shaw <steven@steshaw.org>
2011-10-20 13:32:03 +1000

    CountWords from PinS 17.3

---
+++ CountWords.scala
@@ -0,0 +1,17 @@
+import scala.collection.mutable
+
+def countWords(text: String) = {
+  val counts = mutable.Map.empty[String, Int]
+  for (rawWord <- text.split("[ ,!.]+")) {
+    val word = rawWord.toLowerCase
+    val oldCount =
+      if (counts.contains(word)) counts(word)
+      else 0
+    counts += (word -> (oldCount + 1))
+  }
+  counts
+}
+
+val text = "See Spot run. Run, Spot. Run!"
+
+countWords(text) foreach println
