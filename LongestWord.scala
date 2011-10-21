3893a6123b4f56dcefe03f884e87193cc2eb87b0
Steven Shaw <steven@steshaw.org>
2011-10-21 13:05:25 +1000

    Add benchmarking

--- LongestWord.scala
+++ LongestWord.scala
@@ -13,3 +13,22 @@ def longestWord(words: Array[String]) = {
 
 var longest = longestWord("The quick brown fox".split(" "))
 println(longest)
+
+def time = scala.compat.Platform.currentTime
+
+def timeIt(numTimes: Long)(action: => Unit) = {
+  var start = time
+  var i = 0
+  while (i < numTimes) {
+    action
+    i += 1
+  }
+  time - start
+}
+
+val words = io.Source.fromFile("/usr/share/dict/words").getLines.toArray
+val duration = timeIt(1000) {
+  longest = longestWord(words)
+}
+println("took " + duration + "ms")
+println(longest + " length is " + longest._1.length)
