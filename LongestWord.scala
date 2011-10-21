3053b95578fdf00fc6eb346fa14dd76f67c5f137
Steven Shaw <steven@steshaw.org>
2011-10-21 12:45:53 +1000

    Longest word example from PinS 17.6

---
+++ LongestWord.scala
@@ -0,0 +1,15 @@
+
+def longestWord(words: Array[String]) = {
+  var word = words(0)
+  var idx = 0
+  for (i <- 1 until words.length) {
+    if (words(i).length > word.length) {
+      word = words(i)
+      idx = i
+    }
+  }
+  (word, idx)
+}
+
+var longest = longestWord("The quick brown fox".split(" "))
+println(longest)
