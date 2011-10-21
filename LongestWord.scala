9f3dd721f4e4f36007ecb7f4c5e118e66452e8c5
Steven Shaw <steven@steshaw.org>
2011-10-21 13:08:41 +1000

    Wrap in App to make compilable

--- LongestWord.scala
+++ LongestWord.scala
@@ -1,34 +1,35 @@
-
-def longestWord(words: Array[String]) = {
-  var word = words(0)
-  var idx = 0
-  for (i <- 1 until words.length) {
-    if (words(i).length > word.length) {
-      word = words(i)
-      idx = i
+object LongestWord extends App {
+  def longestWord(words: Array[String]) = {
+    var word = words(0)
+    var idx = 0
+    for (i <- 1 until words.length) {
+      if (words(i).length > word.length) {
+        word = words(i)
+        idx = i
+      }
     }
+    (word, idx)
   }
-  (word, idx)
-}
 
-var longest = longestWord("The quick brown fox".split(" "))
-println(longest)
+  var longest = longestWord("The quick brown fox".split(" "))
+  println(longest)
 
-def time = scala.compat.Platform.currentTime
+  def time = scala.compat.Platform.currentTime
 
-def timeIt(numTimes: Long)(action: => Unit) = {
-  var start = time
-  var i = 0
-  while (i < numTimes) {
-    action
-    i += 1
+  def timeIt(numTimes: Long)(action: => Unit) = {
+    var start = time
+    var i = 0
+    while (i < numTimes) {
+      action
+      i += 1
+    }
+    time - start
   }
-  time - start
-}
 
-val words = io.Source.fromFile("/usr/share/dict/words").getLines.toArray
-val duration = timeIt(1000) {
-  longest = longestWord(words)
+  val words = io.Source.fromFile("/usr/share/dict/words").getLines.toArray
+  val duration = timeIt(1000) {
+    longest = longestWord(words)
+  }
+  println("took " + duration + "ms")
+  println(longest + " length is " + longest._1.length)
 }
-println("took " + duration + "ms")
-println(longest + " length is " + longest._1.length)
