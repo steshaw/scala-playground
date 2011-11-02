cacd110c5d4617ff1a15b8058d46faa658f049da
Steven Shaw <steven@steshaw.org>
2011-11-02 17:47:02 +1000

    Example of interesting recursive-referencing case class bug

---
+++ Robot.scala
@@ -0,0 +1,28 @@
+//
+// Adapted from http://lizdouglass.wordpress.com/2009/12/08/passing-by-name-in-scala/
+//
+
+class Robot(var xPosition: Int, var yPosition: Int, var direction: Direction) {
+  def turnRight(): Unit = {
+    direction = direction.rightDirection;
+  }
+
+  //other methods omitted
+
+  override def toString(): String = {
+    "%d %d %s".format(xPosition, yPosition, direction.abbreviation)
+  }
+}
+
+sealed abstract case class Direction(abbreviation: Char, val leftDirection: Direction, val rightDirection: Direction)
+case object North extends Direction('N', West, East)
+case object West extends Direction('W', South, North)
+case object East extends Direction('E', North, South)
+case object South extends Direction('S', East, West)
+
+object RobotDemo extends App {
+  for (d <- List(North, West, East, South)) {
+    printf("%s left=%s right=%s\n", d, d.leftDirection, d.rightDirection)
+  }
+}
+//RobotDemo.main(Array())
