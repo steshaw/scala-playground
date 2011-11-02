e94876029cb031f7dc0290923811846ed87c08e0
Steven Shaw <steven@steshaw.org>
2011-11-02 18:52:15 +1000

    Robot call-by-name version

---
+++ RobotCallByName.scala
@@ -0,0 +1,27 @@
+//
+// Adapted from http://lizdouglass.wordpress.com/2009/12/08/passing-by-name-in-scala/
+//
+
+class Robot(var xPosition: Int, var yPosition: Int, var direction: Direction) {
+  def turnRight() {
+    direction = direction.rightDirection
+  }
+
+  override def toString(): String = "%d %d %s".format(xPosition, yPosition, direction.abbreviation)
+}
+
+sealed abstract class Direction(val abbreviation: Char, left: => Direction, right: => Direction) {
+  def leftDirection = left
+  def rightDirection = right
+}
+case object North extends Direction('N', West, East)
+case object West extends Direction('W', South, North)
+case object East extends Direction('E', North, South)
+case object South extends Direction('S', East, West)
+
+object RobotDemo extends App {
+  for (d <- List(North, West, East, South)) {
+    printf("%s left=%s right=%s\n", d, d.leftDirection, d.rightDirection)
+  }
+}
+//RobotDemo.main(Array())
