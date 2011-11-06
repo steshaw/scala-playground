aad0816b01aa2091c41325af2b0cb4043b0ecdec
Steven Shaw <steven@steshaw.org>
2011-11-06 17:12:50 +1000

    Implemented concat, concatr and max

--- Excercises.scala
+++ Excercises.scala
@@ -2,8 +2,10 @@
 // Excercises from http://rafs-blog.blogspot.com/2008/01/scala-exercises.html
 //
 
+//
 // map in terms of foldRight
-def map[A, B](f: A => B)(xs: List[A]) = xs.foldRight(Nil : List[B])((n, acc) => f(n) :: acc)
+//
+def map[A, B](f: A => B)(xs: List[A]) = xs.foldRight(Nil: List[B])((n, acc) => f(n) :: acc)
 
 {
   val r = map(((_:Int)+1))(List(1,2,3))
@@ -12,15 +14,72 @@ def map[A, B](f: A => B)(xs: List[A]) = xs.foldRight(Nil : List[B])((n, acc) => 
 
 {
   val r = try {
-    map(((_:Int)+1))((1 to 5000).toList).toString
+    map(((_:Int)+1))((1 to 4500).toList).toString
   } catch {
     case e: StackOverflowError => "blew the stack!"
   }
   println(r)
 }
 
-// TODO concat in terms of foldRight
+//
+// TODO flatMap in terms of foldRight
+//
 
-// TODO concat using recursion
+//
+// concat in terms of foldRight
+//
+def concat[A](xs: List[A], ys: List[A]): List[A] = xs.foldRight(ys)((n, acc) => n :: acc)
 
-// TODO find max Int in list
+{
+  val r = concat(List(1,2,3), List(4,5,6))
+  println(r)
+}
+{
+  val r = try {
+    concat((1 to 4500).toList, List(4,5,6)).toString
+  } catch {
+    case e: StackOverflowError => "blew the stack!"
+  }
+  println(r)
+}
+
+//
+// concat using recursion
+//
+def concatr[A](xs: List[A], ys: List[A]): List[A] = xs match {
+  case Nil => ys
+  case x :: xs => x :: concatr(xs, ys)
+}
+
+{
+  val r = concatr(List(1,2,3), List(4,5,6))
+  println(r)
+}
+{
+  val r = try {
+    concatr((1 to 7000).toList, List(4,5,6)).toString
+  } catch {
+    case e: StackOverflowError => "blew the stack!"
+  }
+  println(r)
+}
+
+//
+// find max Int in list - generalised to T viewable as an Ordered[T].
+//
+def max[T <% Ordered[T]](xs: List[T]) = xs match {
+  case Nil => sys.error("cannot take the max of an empty list")
+  case x :: xs => xs.foldLeft(x)((acc, n) => if (n > acc) n else acc)
+}
+{
+  val r = max(List(6,3,1,9,8))
+  println(r)
+}
+{
+  val r = try {
+    max(scala.util.Random.shuffle((1 to 7000).toList)).toString
+  } catch {
+    case e: StackOverflowError => "blew the stack!"
+  }
+  println(r)
+}
