a7f91deb5a2cbab41d3d54b18ce5c507ee26589e
Steven Shaw <steven@steshaw.org>
2011-11-09 10:37:14 +1000

    Use loop

--- programming-in-scala/NameResolver.scala
+++ programming-in-scala/NameResolver.scala
@@ -1,18 +1,20 @@
 import scala.actors.Actor
+import Actor._
 
 object NameResolver extends Actor {
   import java.net.{InetAddress, UnknownHostException}
 
   def act() {
-    react {
-      case (name: String, actor: Actor) => 
-        actor ! getIp(name)
-        act()
-      case "EXIT" => 
-        println("Name resolver exiting.")
-      case msg =>
-        printf("Unhandled message %s\n", msg)
-        act()
+    loop {
+      react {
+        case (name: String, actor: Actor) => 
+          actor ! getIp(name)
+        case "EXIT" => 
+          println("Name resolver exiting.")
+          throw new RuntimeException("I'm outta here")
+        case msg =>
+          printf("Unhandled message %s\n", msg)
+      }
     }
   }
 
