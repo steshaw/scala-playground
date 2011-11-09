29b53cbd4ef01a58bb8e80b557398e5467c03e3e
Steven Shaw <steven@steshaw.org>
2011-11-09 16:35:41 +1000

    Sprinkle LL(1) shrieks in the Json parser

--- programming-in-scala/Json.scala
+++ programming-in-scala/Json.scala
@@ -3,13 +3,9 @@ import java.io.FileReader
 
 class Json extends JavaTokenParsers {
   def value: Parser[Any] = obj | arr | stringLiteral | floatingPointNumber | "null" | "true" | "false"
-
-  def obj: Parser[Any] = "{" ~ repsep(member, ",") ~ "}"
-
-  def arr: Parser[Any] = "[" ~ repsep(value, ",") ~ "]"
-
-  def member: Parser[Any] = stringLiteral ~ ":" ~ value
-
+  def obj: Parser[Any] = "{" ~! repsep(member, ",") ~! "}"
+  def arr: Parser[Any] = "[" ~! repsep(value, ",") ~! "]"
+  def member: Parser[Any] = stringLiteral ~! ":" ~ value
 }
 
 object ParseJson extends Json {
