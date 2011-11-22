d55e37af1806ebce5dfcec18a449cc84b375fc14
Steven Shaw <steven@steshaw.org>
2011-11-22 11:08:18 +1000

    Correct spelling

--- programming-in-scala/TempConverter.scala
+++ programming-in-scala/TempConverter.scala
@@ -5,23 +5,23 @@ object TempConverter extends SimpleSwingApplication {
   def top = new MainFrame {
     title = "Celcius/Fahrenheit Converter"
     object celcius extends TextField { columns = 5 }
-    object farenheight extends TextField { columns = 5 }
+    object farenheit extends TextField { columns = 5 }
     contents = new FlowPanel {
       contents += celcius
       contents += new Label(" Celsius  =  ")
-      contents += farenheight
+      contents += farenheit
       contents += new Label(" Farenheight")
       border = Swing.EmptyBorder(15, 10, 10, 10)
-      listenTo(celcius, farenheight)
+      listenTo(celcius, farenheit)
       reactions += {
-        case EditDone(`farenheight`) =>
-          val f = farenheight.text.toInt
+        case EditDone(`farenheit`) =>
+          val f = farenheit.text.toInt
           val c = (f - 32) * 5 / 9
           celcius.text = c.toString
         case EditDone(`celcius`) =>
           val c = celcius.text.toInt
           val f = c * 9 / 5 + 32
-          farenheight.text = f.toString
+          farenheit.text = f.toString
       }
     }
   }
