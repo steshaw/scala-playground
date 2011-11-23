0359fcc419b188e8d565260b69bcb21dc635b9a1
Steven Shaw <steven@steshaw.org>
2011-11-23 23:24:05 +1000

    Add dependency tracking (aka change propogation)

--- programming-in-scala/scells/Model.scala
+++ programming-in-scala/scells/Model.scala
@@ -1,12 +1,36 @@
 package org.stairwaybook.scells
 
+import swing.Publisher
+
 class Model(val height: Int,  val width: Int) extends Evaluator with Arithmetic {
-  case class Cell(row: Int,  column: Int) {
-    var formula: Formula = Empty
-    def value = evaluate(formula)
+  case class ValueChanged(cell: Cell) extends swing.event.Event
+
+  case class Cell(row: Int,  column: Int) extends Publisher {
+    private var v: Double = 0.0
+    def value: Double = v
+    def value_=(w: Double) {
+      if (!(v == w || v.isNaN && w.isNaN)) {
+        v = w
+        publish(ValueChanged(this))
+      }
+    }
+
+    private var f: Formula = Empty
+    def formula: Formula = f
+    def formula_=(f: Formula) {
+      for (c <- references(formula)) deafTo(c)
+      this.f = f
+      for (c <- references(formula)) listenTo(c)
+      value = evaluate(f)
+    }
+
     override def toString = formula match {
       case Textual(s) => s
       case _ => value.toString
+    }
+
+    reactions += {
+      case ValueChanged(_) => value = evaluate(formula)
     }
   }
 
