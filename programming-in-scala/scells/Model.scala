7026d5e402788e2c2e636250d10532a0e558a291
Steven Shaw <steven@steshaw.org>
2011-11-23 22:56:27 +1000

    Add evaluator to spreadsheet

--- programming-in-scala/scells/Model.scala
+++ programming-in-scala/scells/Model.scala
@@ -1,9 +1,13 @@
 package org.stairwaybook.scells
 
-class Model(val height: Int,  val width: Int) {
+class Model(val height: Int,  val width: Int) extends Evaluator with Arithmetic {
   case class Cell(row: Int,  column: Int) {
     var formula: Formula = Empty
-    override def toString = formula.toString
+    def value = evaluate(formula)
+    override def toString = formula match {
+      case Textual(s) => s
+      case _ => value.toString
+    }
   }
 
   val cells = Array.ofDim[Cell](height, width)
