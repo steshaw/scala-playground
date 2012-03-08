8d70a298092be2eceacce4bb3e0d0b4e8880efdf
Steven Shaw <steven@steshaw.org>
2012-03-08 19:06:24 +1000

    "Extension methods" in Scala. Wrapper object creation is not optimised away

---
+++ UseExtensionMethod.scala
@@ -0,0 +1,8 @@
+//
+// Use an "extension" method.
+//
+// scalac doesn't appear to make an attempt to prevent the creation of the StringOps wrapper.
+//
+class UseExtensionMethod {
+  val s = "scala".capitalize
+}
