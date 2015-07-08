c3ba6343b374a03e6dfa88864006dacebf128c47
Steven Shaw <steven@steshaw.org>
2015-07-08 20:14:14 +1000

    Tried @unchecked but it did not work

--- ResultTypeRequiredT.scala
+++ ResultTypeRequiredT.scala
@@ -3,12 +3,14 @@
 //
 
 import scala.util._
+import scala.unchecked
 
-class Foo {
+class ResultTypeRequiredT {
 
   implicit class FoldableTry[T](tryable: Try[T]) {
     def fold[X](failure: Throwable => X)(success: T => X): X = {
-      tryable match {
+      // Tried to ignore the error with @unchecked but it does not work (probably 'cause it's not a warning).
+      (tryable: @unchecked) match {
         case Success(result) => success(result)
         case Failure(ex) => failure(ex)
       }
