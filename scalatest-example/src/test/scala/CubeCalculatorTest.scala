3b6c48255ef83290cfccc283d00d25e7341281f1
Steven Shaw <steven@steshaw.org>
2021-04-08 17:28:59 +1000

    another test

--- scalatest-example/src/test/scala/CubeCalculatorTest.scala
+++ scalatest-example/src/test/scala/CubeCalculatorTest.scala
@@ -1,5 +1,10 @@
-class CubeCalculatorTest extends org.scalatest.funsuite.AnyFunSuite {
-  test("CubeCalculator.cube") {
+import org.scalatest.funsuite._
+
+class CubeCalculatorTest extends AnyFunSuite {
+  test("CubeCalculator.cube 3 should be 27") {
     assert(CubeCalculator.cube(3) === 27)
+  }
+  test("CubeCalculator.cube 0 should be 0") {
+    assert(CubeCalculator.cube(0) === 0)
   }
 }
