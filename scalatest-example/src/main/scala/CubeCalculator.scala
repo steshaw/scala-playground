a70f9df8f30c086aae8d327fc48ff0f05d0a6286
Steven Shaw <steven@steshaw.org>
2021-04-08 17:46:53 +1000

    Display JDK version and vendor

--- scalatest-example/src/main/scala/CubeCalculator.scala
+++ scalatest-example/src/main/scala/CubeCalculator.scala
@@ -1,9 +1,11 @@
-object CubeCalculator extends App {
+object CubeCalculator {
   def cube(x: Int) = {
     x * x * x
   }
-  def main() = {
+  def main(args: Array[String]) = {
+    println(s"Java VM vendor = ${sys.props("java.vm.vendor")}")
+    println(s"Java version = ${sys.props("java.version")}")
+    println(s"Scala version: ${util.Properties.versionString}")
     println(s"cube(3) = ${cube(3)}")
   }
-  main()
 }
