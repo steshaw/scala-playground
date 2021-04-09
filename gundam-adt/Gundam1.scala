5bb12f3ba6b39c17dd9165e2556270099f98de3f
Steven Shaw <steven@steshaw.org>
2021-04-09 12:48:49 +1000

    Recover from booms

--- gundam-adt/Gundam1.scala
+++ gundam-adt/Gundam1.scala
@@ -1,15 +1,31 @@
+class Boom(s: String) extends Exception(s)
+
 object Gundam1 {
 
-  def face(a: Any): Unit = {}
+  def face(a: Any): Unit = {
+    a match {
+      case a : Int => {
+        if (a < 0) throw new Boom("negative!")
+      }
+      case _ => ()
+    }
+  }
   def start: Unit = {}
   def stop: Unit = {}
-  class Boom extends Exception
-  def triple_backflip = throw new Boom
+  def triple_backflip = throw new Boom("triple backflip")
 
   val north = "hi"
   val east = 5.6
   val south = new Exception("south")
   val west = 1
+
+  def try_it(f: => Unit): Unit = {
+    try {
+      f
+    } catch {
+      case e: Boom => println(e)
+    }
+  }
 
   def go(): Unit = {
     face(north)
@@ -18,15 +34,11 @@ object Gundam1 {
     face(east)
     start
     stop
-    triple_backflip
-    face(-35)
+    try_it(triple_backflip)
+    try_it(face(-35))
   }
 
   def main(args: Array[String]): Unit = {
-    try {
-      go()
-    } catch {
-      case e: Boom => println("boom!")
-    }
+    go()
   }
 }
