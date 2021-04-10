148968f92813f808ed518029efce61c13dca0737
Steven Shaw <steven@steshaw.org>
2021-04-10 18:48:38 +1000

    Address and suppress

--- gundam-adt/gundam1/Gundam.scala
+++ gundam-adt/gundam1/Gundam.scala
@@ -1,23 +1,26 @@
 class Boom(s: String) extends Exception(s)
 
+@SuppressWarnings(Array("org.wartremover.warts.Throw"))
 object Gundam {
 
   def face(a: Any): Unit = {
     a match {
       case a : Int => {
-        if (a < 0) throw new Boom("negative!")
+        if (a < 0)
+          throw new Boom("negative!")
       }
       case _ => ()
     }
   }
   def start: Unit = {}
   def stop: Unit = {}
-  def triple_backflip = throw new Boom("triple backflip")
 
-  val north = "hi"
-  val east = 5.6
-  val south = new Exception("south")
-  val west = 1
+  def triple_backflip: Unit = throw new Boom("triple backflip")
+
+  private val north = "hi"
+  private val east = 5.6
+  private val south = new Exception("south")
+  private val west = 1
 
   def try_it(f: => Unit): Unit = {
     try {
--- gundam-adt/gundam1/build.sbt
+++ gundam-adt/gundam1/build.sbt
@@ -1,5 +1,9 @@
-scalacOptions ++= Seq("-Werror", "-deprecation")
+scalacOptions ++= Seq(
+  "-Xfatal-warnings",
+  "-deprecation"
+)
 
 scalaVersion := "2.13.5"
 
+wartremoverErrors ++= Warts.unsafe
 wartremoverWarnings ++= Warts.all
