7d7075615be14f6905c7e63bcaa335bf6fb0507e
Steven Shaw <steven@steshaw.org>
2023-06-14 20:00:05 +1000

    Remove braces

--- huttons-razor.scala
+++ huttons-razor.scala
@@ -5,10 +5,9 @@ enum Expr:
 import Expr.*
 
 def eval(expr: Expr): Int =
-    expr match {
+    expr match
       case Constant(i) => i
       case Add(l, r) => eval(l) + eval(r)
-    }
 
 def main(args: Array[String]) =
   val expr = Add(Constant(1), Add(Constant(20), Constant(21)))
