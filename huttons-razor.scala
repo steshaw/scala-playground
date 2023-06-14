65e1a110900da6c980418e25a8b07382208b1e7c
Steven Shaw <steven@steshaw.org>
2023-06-14 19:03:47 +1000

    Hutton's razor

---
+++ huttons-razor.scala
@@ -0,0 +1,15 @@
+enum Expr:
+  case Constant(i: Int)
+  case Add(left: Expr, right: Expr)
+
+import Expr.*
+
+def eval(expr: Expr): Int =
+    expr match {
+      case Constant(i) => i
+      case Add(l, r) => eval(l) + eval(r)
+    }
+
+def main(args: Array[String]) =
+  val expr = Add(Constant(1), Add(Constant(20), Constant(21)))
+  println(s"${expr} = ${eval(expr)}")
