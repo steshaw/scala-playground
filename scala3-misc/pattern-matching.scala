7c468bfda46f7d3cfa4cab2f94bff096623120e7
Steven Shaw <steven@steshaw.org>
2023-06-27 17:46:59 +1000

    Implicit conversions

--- scala3-misc/pattern-matching.scala
+++ scala3-misc/pattern-matching.scala
@@ -15,6 +15,31 @@ def matchTest3(value: Any): Unit = value match
   case set: Set[t] => println(s"Found a set: $set")
   case other => println("Unknown value: $other")
 
+def processValue(value: Any): Unit = value match
+  case str: String => println(s"Found a string: $str")
+  case num: Int => println(s"Found an integer: $num")
+  case _ => println("Unknown value")
+
+def given1 =
+  given Conversion[Double, Int] = _.toInt
+  val doubleValue: Double = 3.14
+  processValue(doubleValue) // Output: Found an integer: 3
+
+def given2 =
+  given Conversion[Double, Int] with {
+    def apply(value: Double): Int = value.toInt
+  }
+  val doubleValue: Double = 3.14
+  processValue(doubleValue) // Output: Found an integer: 3
+
+def given3 =
+  given Conversion[Double, Int] with {
+    def apply(value: Double): Int = value.toInt
+  }
+  val doubleValue: Double = 3.14
+  val intValue: Int = doubleValue // Does the implicit conversion.
+  processValue(intValue) // Output: Found an integer: 3
+
 @main def main =
   val list = List(3, 42, "Hello world!", 'x', Nil)
   println("matchTest1")
@@ -23,4 +48,9 @@ def matchTest3(value: Any): Unit = value match
   list.foreach(matchTest2)
   println("matchTest3")
   list.foreach(matchTest3)
+
+  println("given1"); given1
+  println("given2"); given2
+  println("given3"); given3
+
 
