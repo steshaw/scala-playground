ef222973dd3881c0c947d0b504e4645ae069e265
Steven Shaw <steven@steshaw.org>
2023-06-27 16:54:38 +1000

    Pattern binding example

--- scala3-misc/pattern-matching.scala
+++ scala3-misc/pattern-matching.scala
@@ -1,8 +1,19 @@
-@main def main =
-  val value: Any = 42
-
-  value match {
+def matchTest(value: Any) =
+  print("1st: ")
+  value match
     case str: String => println(s"Found a string: $str")
     case num: Int => println(s"Found an integer: $num")
-    case _ => println("Unknown value")
-  }
+    case other => println(s"Unknown value: $other")
+
+  print("2nd: ")
+  value match
+    case str: String => println(s"Found a string: $str")
+    case num @ 42 => println(s"Found the number 42: $num")
+    case other => println(s"Unknown value $other")
+
+@main def main =
+  matchTest(3)
+  matchTest(42)
+  matchTest("Hello World")
+  matchTest('x')
+  matchTest(Nil)
