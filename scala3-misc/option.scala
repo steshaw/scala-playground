e89cf940dd9ebc42a3050cf8430f3a7ad1e95fd6
Steven Shaw <steven@steshaw.org>
2023-06-27 18:59:36 +1000

    More options

--- scala3-misc/option.scala
+++ scala3-misc/option.scala
@@ -1,16 +1,58 @@
-enum Option[+T]:
-  case Some(x: T)
-  case None
+def option1 =
+  enum Option[+T]:
+    case Some(x: T)
+    case None
 
-val n42 = Option.Some(42)
-val none = Option.None
+  import Option.*
+
+  val n42 = Some(42)
+  val none = None
+  {
+    val x: Option[Int] = Some(42)
+    println(x)
+  }
+  {
+    val x: Option[Int] = None
+    println(x)
+  }
+
+def option2 =
+  trait Option[+T]
+  case class Some[T](x: T) extends Option[T]
+  case object None extends Option[Nothing]
+
+  import Option.*
+
+  val n42 = Some(42)
+  val none = None
+  {
+    val x: Option[Int] = Some(42)
+    println(x)
+  }
+  {
+    val x: Option[Int] = None
+    println(x)
+  }
+
+def option3 =
+  trait Option[T]
+  case class Some[T](x: T) extends Option[T]
+  case class None[T]() extends Option[T]
+
+  import Option.*
+
+  val n42 = Some(42)
+  val none = None()
+  {
+    val x: Option[Int] = Some(42)
+    println(x)
+  }
+  {
+    val x: Option[Int] = None()
+    println(x)
+  }
 
 @main def main =
-  {
-    val x: Option[Int] = Option.Some(42)
-    println(x)
-  }
-  {
-    val x: Option[Int] = Option.None
-    println(x)
-  }
+  option1
+  option2
+  option3
