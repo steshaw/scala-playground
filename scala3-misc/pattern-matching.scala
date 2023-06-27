6ed7e6b58ccf8f8583041cbf9a7bd2e27c22b3b5
Steven Shaw <steven@steshaw.org>
2023-06-27 17:29:23 +1000

    More pattern matching

--- scala3-misc/pattern-matching.scala
+++ scala3-misc/pattern-matching.scala
@@ -1,19 +1,26 @@
-def matchTest(value: Any) =
-  print("1st: ")
-  value match
-    case str: String => println(s"Found a string: $str")
-    case num: Int => println(s"Found an integer: $num")
-    case other => println(s"Unknown value: $other")
+def matchTest1(value: Any) = value match
+  case str: String => println(s"Found a string: $str")
+  case num: Int => println(s"Found an integer: $num")
+  case other => println(s"Unknown value: $other")
 
-  print("2nd: ")
-  value match
-    case str: String => println(s"Found a string: $str")
-    case num @ 42 => println(s"Found the number 42: $num")
-    case other => println(s"Unknown value $other")
+// Pattern binding example (use of "@").
+def matchTest2(value: Any) = value match
+  case str: String => println(s"Found a string: $str")
+  case num @ 42 => println(s"Found the number 42: $num")
+  case other => println(s"Unknown value $other")
+
+// Match types.
+def matchTest3(value: Any): Unit = value match
+  case list: List[t] => println(s"Found a list: $list")
+  case set: Set[t] => println(s"Found a set: $set")
+  case other => println("Unknown value: $other")
 
 @main def main =
-  matchTest(3)
-  matchTest(42)
-  matchTest("Hello World")
-  matchTest('x')
-  matchTest(Nil)
+  val list = List(3, 42, "Hello world!", 'x', Nil)
+  println("matchTest1")
+  list.foreach(matchTest1)
+  println("matchTest2")
+  list.foreach(matchTest2)
+  println("matchTest3")
+  list.foreach(matchTest3)
+
